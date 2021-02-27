d303de6811e4c228723683383e1eb0529a8ea953
felipe-basina <felipe.basina@gmail.com>
2021-02-27 09:23:56 -0300

    Adicionado estrutura inicial do projeto.

---
+++ authorize/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to authorize
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ authorize/src/authorize/core.clj
@@ -0,0 +1,7 @@
+(ns authorize.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
