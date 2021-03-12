3d06e3e7d24a80eac565b986617173d4842c2a80
felipe-basina <felipe.basina@gmail.com>
2021-03-12 16:02:42 -0300

    Ajuste em identacao + remocao de rotulo em cabecalho.

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -12,31 +12,31 @@
          [:div.empty
           [:div.title "You don't have any orders"]
           [:div.subtitle "Click on a + to add an order"]])
-       [:div.order
-        [:div.body
-         (for [[id quant] @state/orders]
-              (if (> quant 0)
-                [:div.item {:key      id
-                            :on-click (fn [] (swap! state/orders update id dec))}
-                 [:div.img
-                  [:img {:src (get-in @state/gigs [id :img])
-                         :alt (get-in @state/gigs [id :title])}]]
-                 [:div.content
-                  [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
-                 [:div.action
-                  [:div.price (* (get-in @state/gigs [id :price]) quant)]
-                  [:button.btn.btn--link.tooltip
-                   {:data-tooltip "Remove"
-                    :on-click     (fn [] (swap! state/orders dissoc id))}
-                   [:i.icon.icon--cross]]]]
-                (swap! state/orders dissoc id)))]
-        [:div.total
-         [:hr]
-         [:div.item
-          [:div.content "Total"]
-          [:div.action
-           [:div.price (str "$" (total))]]
-          [:button.btn.btn--link.tooltip
-           {:data-tooltip "Remove all"
-            :on-click     (fn [] (reset! state/orders {}))}
-           [:i.icon.icon--delete]]]]]])
+         [:div.order
+          [:div.body
+           (for [[id quant] @state/orders]
+                (if (> quant 0)
+                  [:div.item {:key      id
+                              :on-click (fn [] (swap! state/orders update id dec))}
+                   [:div.img
+                    [:img {:src (get-in @state/gigs [id :img])
+                           :alt (get-in @state/gigs [id :title])}]]
+                   [:div.content
+                    [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                   [:div.action
+                    [:div.price (* (get-in @state/gigs [id :price]) quant)]
+                    [:button.btn.btn--link.tooltip
+                     {:data-tooltip "Remove"
+                      :on-click     (fn [] (swap! state/orders dissoc id))}
+                     [:i.icon.icon--cross]]]]
+                  (swap! state/orders dissoc id)))]
+          [:div.total
+           [:hr]
+           [:div.item
+            [:div.content "Total"]
+            [:div.action
+             [:div.price (str "$" (total))]]
+            [:button.btn.btn--link.tooltip
+             {:data-tooltip "Remove all"
+              :on-click     (fn [] (reset! state/orders {}))}
+             [:i.icon.icon--delete]]]]]])
