a95010690996a37679d48bdb54cb303c8ca2e7a0
felipe-basina <felipe.basina@gmail.com>
2021-03-12 16:29:03 -0300

    Refatoracao para utilizacao de referencias de funcoes nos eventos onclick.

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,29 +1,30 @@
 (ns giggin.components.gigs
-      (:require [giggin.helpers :refer [format-price]]
-                [giggin.state :as state]))
+  (:require [giggin.helpers :refer [format-price]]
+            [giggin.state :as state]))
 
 (defn gigs []
-      [:main
-       [:div.gigs
-        (for [{:keys [id img title price desc]} (vals @state/gigs)] ; List comprehension
-             [:div.gig {:key id}
-              [:img.gig__artwork {:src img :alt title}]
-              [:div.gig__body
-               [:div.gig__title
-                [:div.btn.btn--primary.float--right.tooltip
-                 {:data-tooltip "Add to order"
-                  :on-click (fn [] (swap! state/orders update id inc))}
-                 [:i.icon.icon--plus]] title]
-               [:p.gig__price (format-price price)]
-               [:p.gig__desc desc]]])]])
+      (let [add-item #(swap! state/orders update % inc)]
+           [:main
+            [:div.gigs
+             (for [{:keys [id img title price desc]} (vals @state/gigs)] ; List comprehension
+                  [:div.gig {:key id}
+                   [:img.gig__artwork {:src img :alt title}]
+                   [:div.gig__body
+                    [:div.gig__title
+                     [:div.btn.btn--primary.float--right.tooltip
+                      {:data-tooltip "Add to order"
+                       :on-click     #(add-item id)}
+                      [:i.icon.icon--plus]] title]
+                    [:p.gig__price (format-price price)]
+                    [:p.gig__desc desc]]])]]))
 
 (comment
-      (map (fn [gig]  ; Using map + anonymous function
-               [:div.gig {:key (:id gig)}
-                [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
-                [:div.gig__body
-                 [:div.gig__title
-                  [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-                   [:i.icon.icon--plus]] (:title gig)]
-                 [:p.gig__price (:price gig)]
-                 [:p.gig__desc (:desc gig)]]]) (vals @state/gigs)))
+  (map (fn [gig]                                            ; Using map + anonymous function
+           [:div.gig {:key (:id gig)}
+            [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
+            [:div.gig__body
+             [:div.gig__title
+              [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
+               [:i.icon.icon--plus]] (:title gig)]
+             [:p.gig__price (:price gig)]
+             [:p.gig__desc (:desc gig)]]]) (vals @state/gigs)))
--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -8,36 +8,39 @@
            (reduce +)))
 
 (defn orders []
-      [:aside
-       (if (empty? @state/orders)
-         [:div.empty
-          [:div.title "You don't have any orders"]
-          [:div.subtitle "Click on a + to add an order"]])
-         [:div.order
-          [:div.body
-           (for [[id quant] @state/orders]
-                (if (> quant 0)
-                  [:div.item {:key      id
-                              :on-click (fn [] (swap! state/orders update id dec))}
-                   [:div.img
-                    [:img {:src (get-in @state/gigs [id :img])
-                           :alt (get-in @state/gigs [id :title])}]]
-                   [:div.content
-                    [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
-                   [:div.action
-                    [:div.price (helper/format-price (* (get-in @state/gigs [id :price]) quant))]
-                    [:button.btn.btn--link.tooltip
-                     {:data-tooltip "Remove"
-                      :on-click     (fn [] (swap! state/orders dissoc id))}
-                     [:i.icon.icon--cross]]]]
-                  (swap! state/orders dissoc id)))]
-          [:div.total
-           [:hr]
-           [:div.item
-            [:div.content "Total"]
-            [:div.action
-             [:div.price (helper/format-price (total))]]
-            [:button.btn.btn--link.tooltip
-             {:data-tooltip "Remove all"
-              :on-click     (fn [] (reset! state/orders {}))}
-             [:i.icon.icon--delete]]]]]])
+      (let [decrement-item #(swap! state/orders update % dec)
+            remove-by-id #(swap! state/orders dissoc %)
+            remove-all #(reset! state/orders {})]
+           [:aside
+            (if (empty? @state/orders)
+              [:div.empty
+               [:div.title "You don't have any orders"]
+               [:div.subtitle "Click on a + to add an order"]])
+            [:div.order
+             [:div.body
+              (for [[id quant] @state/orders]
+                   (if (> quant 0)
+                     [:div.item {:key      id
+                                 :on-click #(decrement-item id)}
+                      [:div.img
+                       [:img {:src (get-in @state/gigs [id :img])
+                              :alt (get-in @state/gigs [id :title])}]]
+                      [:div.content
+                       [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                      [:div.action
+                       [:div.price (helper/format-price (* (get-in @state/gigs [id :price]) quant))]
+                       [:button.btn.btn--link.tooltip
+                        {:data-tooltip "Remove"
+                         :on-click     #(remove-by-id id)}
+                        [:i.icon.icon--cross]]]]
+                     (remove-by-id id)))]
+             [:div.total
+              [:hr]
+              [:div.item
+               [:div.content "Total"]
+               [:div.action
+                [:div.price (helper/format-price (total))]]
+               [:button.btn.btn--link.tooltip
+                {:data-tooltip "Remove all"
+                 :on-click     #(remove-all)}
+                [:i.icon.icon--delete]]]]]]))
