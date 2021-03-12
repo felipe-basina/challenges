05f1c38e541b422313d7e298c759c10728491acc
felipe-basina <felipe.basina@gmail.com>
2021-03-11 21:04:44 -0300

    Refatorado solucao para remover item de pedidos.

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -6,13 +6,15 @@
        [:div.order
         [:div.body
          (for [[id quant] @state/orders]
-              (when (> quant 0)
-                    [:div.item {:key id
-                          :on-click (fn [] (swap! state/orders update id dec))}
+              [:div.item {:key id}
                [:div.img
                 [:img {:src (get-in @state/gigs [id :img])
                        :alt (get-in @state/gigs [id :title])}]]
                [:div.content
                 [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
                [:div.action
-                [:div.price (* (get-in @state/gigs [id :price]) quant)]]]))]]])
+                [:div.price (* (get-in @state/gigs [id :price]) quant)]
+                [:button.btn.btn--link.tooltip
+                 {:data-tooltip "Remove"
+                  :on-click (fn [] (swap! state/orders dissoc id))}
+                 [:i.icon.icon--cross]]]])]]])
