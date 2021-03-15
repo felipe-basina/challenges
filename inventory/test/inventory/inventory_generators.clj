71bc452ad2a29ad84d03d86ddae3d0136a34eff5
felipe-basina <felipe.basina@gmail.com>
2021-03-15 17:07:02 -0300

    Adicionado dependencia + namespace para testes com geradores.

---
+++ inventory/test/inventory/inventory_generators.clj
@@ -0,0 +1,4 @@
+(ns inventory.inventory-generators
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
