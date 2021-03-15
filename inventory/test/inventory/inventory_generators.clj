b211e3e97628d1a87c519a6e11d344d9f9dd4823
felipe-basina <felipe.basina@gmail.com>
2021-03-15 17:14:07 -0300

    Adicionado controle para geracao somente de valores validos: string nao vazia + numero > 0.

--- inventory/test/inventory/inventory_generators.clj
+++ inventory/test/inventory/inventory_generators.clj
@@ -1,13 +1,17 @@
 (ns inventory.inventory-generators
-  (:require [clojure.test :refer :all]
+  (:require [inventory.inventory :as inventory]
+            [clojure.test :refer :all]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
-(def title-gen gen/string-alphanumeric)
+;(def title-gen gen/string-alphanumeric)
+(def title-gen (gen/such-that not-empty gen/string-alphanumeric))
 (println (gen/sample title-gen))
 
-(def author-gen gen/string-alphanumeric)
+;(def author-gen gen/string-alphanumeric)
+(def author-gen (gen/such-that not-empty gen/string-alphanumeric))
 (println (gen/sample author-gen))
 
-(def copies-gen gen/nat)
+;(def copies-gen gen/nat)
+(def copies-gen (gen/such-that (complement zero?) gen/nat))
 (println (gen/sample copies-gen))
