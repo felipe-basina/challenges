c84fc1ada0b40e6d0375461066aaad0c81ccb937
felipe-basina <felipe.basina@gmail.com>
2021-03-15 17:10:40 -0300

    Adicionado geradores para titulo, autor e nro. copias.

--- inventory/test/inventory/inventory_generators.clj
+++ inventory/test/inventory/inventory_generators.clj
@@ -2,3 +2,12 @@
   (:require [clojure.test :refer :all]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
+
+(def title-gen gen/string-alphanumeric)
+(println (gen/sample title-gen))
+
+(def author-gen gen/string-alphanumeric)
+(println (gen/sample author-gen))
+
+(def copies-gen gen/nat)
+(println (gen/sample copies-gen))
