ec7e8ce581a824c4628bc7217e9f93081e89d215
felipe-basina <felipe.basina@gmail.com>
2021-03-15 17:40:59 -0300

    Adicionado funcao para gerar n registros + realizar validacao.

--- inventory/test/inventory/inventory_generators.clj
+++ inventory/test/inventory/inventory_generators.clj
@@ -1,6 +1,7 @@
 (ns inventory.inventory-generators
   (:require [inventory.inventory :as inventory]
             [clojure.test :refer :all]
+            [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
@@ -20,6 +21,19 @@
   (gen/hash-map :title title-gen :author author-gen :copies copies-gen))
 (println (gen/sample book-gen))
 
-(def inventory-gen
-  (gen/not-empty (gen/vector book-gen)))
+(def inventory-gen (gen/not-empty (gen/vector book-gen)))
 (println (gen/sample inventory-gen))
+
+(def inventory-and-book-gen
+  (gen/let [inventory inventory-gen
+            book (gen/elements inventory)]
+           {:inventory inventory :book book}))
+(println "\nInventory and book generated" (gen/sample inventory-and-book-gen))
+
+(tc/quick-check 50
+                (prop/for-all [i-and-b inventory-and-book-gen]
+                              (= (inventory/find-by-title (-> i-and-b
+                                                              :book
+                                                              :title)
+                                                          (:inventory i-and-b))
+                                 (:book i-and-b))))
