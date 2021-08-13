57804dddc83cbd84d274ed66bff5ac0f02a6aa59
felipe-basina <felipe.basina@gmail.com>
2021-08-12 22:03:43 -0300

    Adicionado formatacao de preco.

--- giggin.pro/src/giggin/components/orders.cljs
+++ giggin.pro/src/giggin/components/orders.cljs
@@ -1,5 +1,6 @@
 (ns giggin.components.orders
-  (:require [giggin.state :as state]))
+  (:require [giggin.state :as state]
+            [giggin.helpers :refer [format-price]]))
 
 (comment defn total []
          ;; Destructuring the id, qtd from each order
@@ -15,28 +16,32 @@
 (defn orders
       []
       [:aside
-       [:div.order
-        [:div.body
-         (for [[id quant] @state/orders]
-              [:div.item {:key id}
-               [:div.img
-                [:img {:src (get-in @state/gigs [id :img])
-                       :alt (get-in @state/gigs [id :title])}]]
-               [:div.content
-                [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
-               [:div.action
-                [:div.price (* (get-in @state/gigs [id :price]) quant)]
-                [:button.btn.btn--link.tooltip
-                 {:data-tooltip "Remove"
-                  :on-click     #(swap! state/orders dissoc id)}
-                 [:i.icon.icon--cross]]]])]
-        [:div.total
-         [:hr]
-         [:div.item
-          [:div.content "Total"]
-          [:div.action
-           [:div.price (total)]]
-          [:button.btn.btn--link.tooltip
-           {:data-tooltip "Remove all"
-            :on-click     #(reset! state/orders {})}
-           [:i.icon.icon--delete]]]]]])
+       (if (empty? @state/orders)
+         [:div.empty
+          [:div.title "You don't have any orders"]
+          [:div.subtitle "Click on a + to add an order"]]
+         [:div.order
+          [:div.body
+           (for [[id quant] @state/orders]
+                [:div.item {:key id}
+                 [:div.img
+                  [:img {:src (get-in @state/gigs [id :img])
+                         :alt (get-in @state/gigs [id :title])}]]
+                 [:div.content
+                  [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                 [:div.action
+                  [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
+                  [:button.btn.btn--link.tooltip
+                   {:data-tooltip "Remove"
+                    :on-click     #(swap! state/orders dissoc id)}
+                   [:i.icon.icon--cross]]]])]
+          [:div.total
+           [:hr]
+           [:div.item
+            [:div.content "Total"]
+            [:div.action
+             [:div.price (format-price (total))]]
+            [:button.btn.btn--link.tooltip
+             {:data-tooltip "Remove all"
+              :on-click     #(reset! state/orders {})}
+             [:i.icon.icon--delete]]]]])])
