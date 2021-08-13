93812c8b4c7c8c7af1c9c057648b029d0956a676
felipe-basina <felipe.basina@gmail.com>
2021-08-12 22:21:04 -0300

    Refatoracao de funcoes para utilizar simbolos associadas as funcoes anonimas.

--- giggin.pro/src/giggin/components/gigs.cljs
+++ giggin.pro/src/giggin/components/gigs.cljs
@@ -5,20 +5,21 @@
 ;; This approach relies on list comprehension
 (defn gigs
       []
-      [:main
-       ;; Using destructuring to get the items from the map
-       ;; So it is not needed to get values from map as (:keyword my-map)
-       [:div.gigs (for [{:keys [id img title price desc]} (vals @state/gigs)]
-                        [:div.gig {:key id}
-                         [:img.gig__artwork {:src img :alt title}]
-                         [:div.gig__body
-                          [:div.gig__title
-                           [:div.btn.btn--primary.float--right.tooltip
-                            {:data-tooltip "Add to order"
-                             :on-click #(swap! state/orders update id inc)}
-                            [:i.icon.icon--plus]] title]
-                          [:p.gig__price (format-price price)]
-                          [:p.gig__desc desc]]])]])
+      (let [add-to-order #(swap! state/orders update % inc)]
+           [:main
+            ;; Using destructuring to get the items from the map
+            ;; So it is not needed to get values from map as (:keyword my-map)
+            [:div.gigs (for [{:keys [id img title price desc]} (vals @state/gigs)]
+                            [:div.gig {:key id}
+                             [:img.gig__artwork {:src img :alt title}]
+                             [:div.gig__body
+                              [:div.gig__title
+                               [:div.btn.btn--primary.float--right.tooltip
+                                {:data-tooltip "Add to order"
+                                 :on-click     #(add-to-order id)}
+                                [:i.icon.icon--plus]] title]
+                              [:p.gig__price (format-price price)]
+                              [:p.gig__desc desc]]])]]))
 
 ;; This approach relies on map function
 (comment
