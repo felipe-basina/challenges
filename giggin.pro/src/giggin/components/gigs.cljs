0a20ad7123a163f57c2b400bbf5cdcd970e3bed3
felipe-basina <felipe.basina@gmail.com>
2021-08-19 21:28:31 -0300

    Refatoracao para permitir inserir e alterar novo item.

--- giggin.pro/src/giggin/components/gigs.cljs
+++ giggin.pro/src/giggin/components/gigs.cljs
@@ -5,38 +5,47 @@
             [reagent.core :as r]
             [clojure.string :as str]))
 
+;; Get default images: https://source.unsplash.com/random/400x400
+
 ;; This approach relies on list comprehension
 (defn gigs
       []
-      (let [modal (r/atom false)
-            values (r/atom {:id       (str "gig-" (random-uuid))
+      (let [modal (r/atom {:active false})
+            values (r/atom {:id       nil
                             :title    ""
                             :desc     ""
                             :price    0
                             :img      ""
                             :sold-out false})
             add-to-order #(swap! state/orders update % inc)
-            insert-gig (fn [{:keys [id title desc price img sold-out]}]
-                           (swap! state/gigs assoc id {:id       id
+            toggle-modal (fn [{:keys [active gig]}]
+                             (swap! modal assoc :active active)
+                             (reset! values gig))
+            upsert-gig (fn [{:keys [id title desc price img sold-out]}]
+                           (swap! state/gigs assoc id {:id       (or id (str "gig-" (random-uuid))) ; Use 'id' if it exists, otherwise generates a new id
                                                        :title    (str/trim title)
                                                        :desc     (str/trim desc)
                                                        :price    (js/parseInt price)
                                                        :img      (str/trim img)
                                                        :sold-out sold-out})
-                           (reset! modal false))]
+                           (toggle-modal {:active false :gig {}}))]
            [:main
             [:div.gigs
              [:button.add-gig
-              {:on-click #(reset! modal true)}
+              {:on-click #(toggle-modal {:active true :gig {}})}
               [:div.add__title
                [:i.icon.icon--plus]
                [:p "Add gig"]]]
-             [gig-editor modal values insert-gig]
+             [gig-editor modal values upsert-gig toggle-modal]
              ;; Using destructuring to get the items from the map
              ;; So it is not needed to get values from map as (:keyword my-map)
-             (for [{:keys [id img title price desc]} (vals @state/gigs)]
-                            [:div.gig {:key id}
-                             [:img.gig__artwork {:src img :alt title}]
+             ;; and using :as we can get the full map as well
+             (for [{:keys [id img title price desc] :as gig} (vals @state/gigs)]
+                  [:div.gig {:key id}
+                   [:img.gig__artwork.gig__edit {:src      img
+                                                 :alt      title
+                                                 :on-click #(toggle-modal {:active true
+                                                                           :gig gig})}]
                              [:div.gig__body
                               [:div.gig__title
                                [:div.btn.btn--primary.float--right.tooltip
