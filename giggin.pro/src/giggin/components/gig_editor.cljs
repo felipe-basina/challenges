226e5239adefb622296d8f874781402ce4e0499e
felipe-basina <felipe.basina@gmail.com>
2021-08-19 21:27:54 -0300

    Refatoracao de nomenclatura de funcao para inserir e atualizar item.

--- giggin.pro/src/giggin/components/gig_editor.cljs
+++ giggin.pro/src/giggin/components/gig_editor.cljs
@@ -15,8 +15,8 @@
                             :on-change #(swap! values assoc (keyword id) (.. % -target -value))}]])
 
 (defn gig-editor
-      [modal values insert-gig]
-      [:div.modal (when @modal {:class "active"}) ; when "modal" is available set the class "active
+      [modal values upsert-gig toggle-modal]
+      [:div.modal (when (:active @modal) {:class "active"}) ; when "modal" is available set the class "active
        [:div.modal__overlay]
        [:div.modal__container
         [:div.modal__body
@@ -33,8 +33,8 @@
           [:i.form__icon]]]
        [:div.modal__footer
         [:button.btn.btn--link.float--left
-         {:on-click #(reset! modal false)}
+         {:on-click #(toggle-modal {:active false :gig {}})}
          "Cancel"]
         [:button.btn.btn--secondary
-         {:on-click #(insert-gig @values)}
+         {:on-click #(upsert-gig @values)}
          "Save"]]]])
