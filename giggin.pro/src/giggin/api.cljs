e335b28259061f946812e1e83180472280733e3e
felipe-basina <felipe.basina@gmail.com>
2021-08-21 14:16:08 -0300

    Adicionado tratamento para retorno de chamada a api externa para recuperacao dos itens.

--- giggin.pro/src/giggin/api.cljs
+++ giggin.pro/src/giggin/api.cljs
@@ -1,10 +1,36 @@
 (ns giggin.api
-  (:require (ajax.core :refer [GET])))
+  (:require [ajax.core :refer [GET]]
+            [giggin.state :as state]))
 
 (def URL "https://gist.githubusercontent.com/jacekschae/6f449ccd07d78740c6dcb54b07a3d5bc/raw/c94e3c21ea39248adb5c23636a0f5fe3d9a4f5f9/gigs.json")
 
+(defn index-by
+      [key collection]
+      "Transform a coll to a map with a given key as a lookup value"
+      (->> collection
+           (map (juxt key identity))
+           (into {})))
+
+;; Using the walk/keywordize-keys
+;; to transform the string id into a clojure keyword
+;; Needs to import [clojure.walk :as walk]
+(comment
+  (defn index-by
+      [key collection]
+      "Transform a coll to a map with a given key as a lookup value"
+      (->> collection
+           (map (juxt key identity))
+           (into {})
+           (walk/keywordize-keys))))
+
+;; With thread (last) macro
 (defn handler [response]
-      (.log js/console response))
+      (reset! state/gigs (index-by :id response)))
+
+;; Without thread macro
+(comment
+  (defn handler [response]
+      (reset! state/gigs (walk/keywordize-keys (into {} (map (juxt :id identity) response))))))
 
 (defn error-handler [{:keys [status status-text]}]
       (.log js/console (str "something bad happened: " status " " status-text)))
