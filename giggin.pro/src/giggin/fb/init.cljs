d777692858cfd8753999fd60477196d20b8cac87
felipe-basina <felipe.basina@gmail.com>
2021-08-25 07:46:32 -0300

    Ajustes para inicializacao da aplicacao no firebase.

--- giggin.pro/src/giggin/fb/init.cljs
+++ giggin.pro/src/giggin/fb/init.cljs
@@ -3,11 +3,13 @@
             ["firebase/database"]
             ["firebase/auth"]))
 
-(defn firebase-init []
-      (firebase/initializeApp {:apiKey            "AIzaSyDkeXUELMS7LAL7neoowkb6ZpPG5M1Fv2I"
-                               :authDomain        "giggin-5507e.firebaseapp.com"
-                               :projectId         "giggin-5507e"
-                               :storageBucket     "giggin-5507e.appspot.com"
-                               :messagingSenderId "282567272849"
-                               :appId             "1:282567272849:web:a4fe8f33e5db85244e1230"
-                               :measurementId     "G-F0N7CDFN91"}))
+(defn firebase-init
+      []
+      "Controls the firebase initialization"
+      ;; (cjs -> js or #js - to send a map as JS object
+      (if (zero? (alength firebase/apps))
+        (firebase/initializeApp #js {:apiKey      "AIzaSyCskb4dbV6zQTinojacpC2SUPRBDx2zZlM"
+                                     :authDomain  "poc-login-1.firebaseapp.com"
+                                     :databaseURL "https://poc-login-1.firebaseio.com"
+                                     :projectId   "poc-login-1"})
+        (firebase/app)))
