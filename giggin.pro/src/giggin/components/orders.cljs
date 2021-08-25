9cfaa0b9e7dc4fb91106f028001fba4ffcb15b85
felipe-basina <felipe.basina@gmail.com>
2021-08-25 07:45:42 -0300

    Adicionado botao para publicar itens no firebase.

--- giggin.pro/src/giggin/components/orders.cljs
+++ giggin.pro/src/giggin/components/orders.cljs
@@ -1,7 +1,8 @@
 (ns giggin.components.orders
   (:require [giggin.state :as state]
             [giggin.helpers :refer [format-price]]
-            [giggin.components.checkout-modal :refer [checkout-modal]]))
+            [giggin.components.checkout-modal :refer [checkout-modal]]
+            [giggin.components.admin-panel :refer [admin-panel]]))
 
 (comment defn total []
          ;; Destructuring the id, qtd from each order
@@ -19,6 +20,7 @@
       (let [remove-from-order #(swap! state/orders dissoc %)
             remove-all-orders #(reset! state/orders {})]
            [:aside
+            [admin-panel]
             (if (empty? @state/orders)
               [:div.empty
                [:div.title "You don't have any orders"]
