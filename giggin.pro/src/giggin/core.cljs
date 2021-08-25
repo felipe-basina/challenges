fd34132ba9dcd4eecda714dc29a7b9b88302b0e8
felipe-basina <felipe.basina@gmail.com>
2021-08-24 23:45:37 -0300

    Adicionado chamada para a funcao de teste de conexao firebase.

--- giggin.pro/src/giggin/core.cljs
+++ giggin.pro/src/giggin/core.cljs
@@ -4,24 +4,26 @@
             [giggin.components.gigs :refer [gigs]]
             [giggin.components.orders :refer [orders]]
             [giggin.components.footer :refer [footer]]
-            [giggin.api :as api]))
+            [giggin.api :as api]
+            [giggin.fb.init :refer [firebase-init]]))
 
 ;; We can also define the div like this
 ;; [:div {:class "container"}]
 ;; [:div {:id "my-div"}]
 (defn app
-  []
-  [:div.container
-   [header]
-   [gigs]
-   [orders]
-   [footer]])
+      []
+      [:div.container
+       [header]
+       [gigs]
+       [orders]
+       [footer]])
 
 ;; The export macro allows the main function
 ;; to be available at the HTML file
 (defn ^:export main
-  []
-  (api/fetch-gigs)
-  (r/render
-    [app]
-    (.getElementById js/document "app")))
+      []
+      (api/fetch-gigs)
+      (r/render
+        [app]
+        (.getElementById js/document "app"))
+      (firebase-init))
