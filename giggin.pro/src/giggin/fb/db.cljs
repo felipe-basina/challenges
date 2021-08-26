e061886c1d9c3bf91d0247c458e96333a1f330f9
felipe-basina <felipe.basina@gmail.com>
2021-08-25 21:29:59 -0300

    Adicionado funcao para recuperar dados do firebase.

--- giggin.pro/src/giggin/fb/db.cljs
+++ giggin.pro/src/giggin/fb/db.cljs
@@ -1,6 +1,7 @@
 (ns giggin.fb.db
   (:require ["firebase/app" :refer [database]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [giggin.state :as state]))
 
 (defn db-ref
       [path]
@@ -9,3 +10,10 @@
 (defn db-save!
       [path value]
       (.set (db-ref path) value))
+
+(defn db-subscribe
+      [path]
+      (.on (db-ref path)
+           "value"
+           (fn [snapshot]
+               (reset! state/gigs (js->clj (.val snapshot) :keywordize-keys true))))) ;; js->clj convert from js to cljs
