c89692a17eaaea25700f0a20a0033f215579ea73
felipe-basina <felipe.basina@gmail.com>
2021-08-25 21:51:35 -0300

    Refatoracao para controlar exibicao de elementos quando item estiver esgotado.

--- giggin.pro/src/giggin/components/orders.cljs
+++ giggin.pro/src/giggin/components/orders.cljs
@@ -12,7 +12,10 @@
 ;; Calculating the total using thread last
 (defn total []
       (->> @state/orders
-           (map (fn [[id qtd]] (* qtd (get-in @state/gigs [id :price]))))
+           (map (fn [[id qtd]]
+                    (if (get-in @state/gigs [id :sold-out])
+                      0
+                      (* qtd (get-in @state/gigs [id :price])))))
            (reduce +)))
 
 (defn orders
@@ -33,9 +36,13 @@
                        [:img {:src (get-in @state/gigs [id :img])
                               :alt (get-in @state/gigs [id :title])}]]
                       [:div.content
-                       [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                       (if (get-in @state/gigs [id :sold-out])
+                         [:p.sold-out "Sold out"]
+                         [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)])]
                       [:div.action
-                       [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
+                       (if (get-in @state/gigs [id :sold-out])
+                         [:div.price (format-price 0)]
+                         [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))])
                        [:button.btn.btn--link.tooltip
                         {:data-tooltip "Remove"
                          :on-click     #(remove-from-order id)}
