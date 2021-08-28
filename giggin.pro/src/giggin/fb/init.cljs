3d3a55c01b0448cf0e0d06576bf0dad9cb6f9cfc
felipe-basina <felipe.basina@gmail.com>
2021-08-27 21:42:00 -0300

    Comentado verificacao de inicializacao do firebase, uma fez que foi feito a refatoracao que garante que a funcao para iniciar o firebase ocorra apenas uma unica vez, no momento do inicio da aplicacao.

--- giggin.pro/src/giggin/fb/init.cljs
+++ giggin.pro/src/giggin/fb/init.cljs
@@ -8,10 +8,13 @@
       []
       "Controls the firebase initialization"
       ;; (cjs -> js or #js - to send a map as JS object
-      (if (zero? (alength firebase/apps))
-        (firebase/initializeApp #js {:apiKey      "AIzaSyCskb4dbV6zQTinojacpC2SUPRBDx2zZlM"
-                                     :authDomain  "poc-login-1.firebaseapp.com"
-                                     :databaseURL "https://poc-login-1.firebaseio.com"
-                                     :projectId   "poc-login-1"})
+      (comment
+        ;; As the creation of function (after-load start)
+        ;; the application will load/create the firebase connection just once
+        if (zero? (alength firebase/apps))
         (firebase/app))
+      (firebase/initializeApp #js {:apiKey      "AIzaSyCskb4dbV6zQTinojacpC2SUPRBDx2zZlM"
+                                   :authDomain  "poc-login-1.firebaseapp.com"
+                                   :databaseURL "https://poc-login-1.firebaseio.com"
+                                   :projectId   "poc-login-1"})
       (on-auth-state-changed))
